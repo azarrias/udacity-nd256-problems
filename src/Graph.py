d409409719123d9336b972e7cfbb224d07886f10
azarrias <adolfo.zarrias@gmail.com>
2020-06-22 11:18:02 +0200

    Add Graph class and tiny example using it

---
+++ src/Graph.py
@@ -0,0 +1,21 @@
+# Helper Code
+from collections import defaultdict
+class Graph:
+    def __init__(self):
+        self.nodes = set()                   # A set cannot contain duplicate nodes
+        self.neighbours = defaultdict(list)  # Defaultdict is a child class of Dictionary that provides a default value for a key that does not exists.
+        self.distances = {}                  # Dictionary. An example record as ('A', 'B'): 6 shows the distance between 'A' to 'B' is 6 units
+
+    def add_node(self, value):
+        self.nodes.add(value)
+
+    def add_edge(self, from_node, to_node, distance):
+        self.neighbours[from_node].append(to_node)
+        self.neighbours[to_node].append(from_node)
+        self.distances[(from_node, to_node)] = distance
+        self.distances[(to_node, from_node)] = distance    # lets make the graph undirected / bidirectional 
+        
+    def print_graph(self):
+        print("Set of Nodes are: ", self.nodes)
+        print("Neighbours are: ", self.neighbours)
+        print("Distances are: ", self.distances)
---
+++ src/dijkstra.py
@@ -0,0 +1,6 @@
+from Graph import Graph
+
+if __name__ == '__main__':
+    g = Graph()
+    g.add_node(5)
+    g.print_graph()
